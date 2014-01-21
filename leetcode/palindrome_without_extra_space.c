0fa5aaf55db4be1c51325e193a0dd3d458cc6f80
indradhanush <indradhanush.gupta@gmail.com>
2014-01-21 21:31:12 +0530

    Added palindrome code.

---
+++ leetcode/palindrome_without_extra_space.c
@@ -0,0 +1,50 @@
+/* Determine whether an integer is a palindrome. Do this without extra space. */
+/* http://oj.leetcode.com/problems/palindrome-number/ */
+
+#include <stdio.h>
+#include <math.h>
+
+
+int isPalindrome(int);
+
+
+int main(){
+    int x, ret;
+    scanf("%d", &x);
+    ret = isPalindrome(x);
+    if (ret == 1)
+        printf("True\n");
+    else
+        printf("False\n");
+    return 0;
+}
+
+
+int isPalindrome(int x){
+    int mod = 10, left, right;
+    if (x < 0)
+        return 0;
+    if (x < 10)
+        return 1;
+    else{
+        while (x/mod >= 10)
+            mod *= 10;
+        
+        while (x >= 10){
+            left = x / mod;
+            right = x % 10;
+            if (left != right)
+                return 0;
+
+            x %= mod;
+            x /= 10;
+            mod /= 100;
+        }
+        return 1;
+        
+    }
+        
+
+
+    
+}
